be8cb2c2e14d657aa215c41b73f480bd89d67542
Deskind <desk1nd88@gmail.com>
2016-05-05 23:58:03 +0300

    user sign in

---
+++ signIn.php
@@ -0,0 +1,19 @@
+<?php
+    $host = 'localhost';
+    $dbname = 'howyoudoing';
+    $username = 'root';
+    $password = '';
+    
+    try {
+        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    } catch (PDOException $pe) {
+        echo $pe->getMessage();
+    }
+    
+    $country =  $_GET['country'];
+    $name = $_GET['name'];
+    $password = $_GET['password'];
+    
+    $signInUserQuery = "insert into user (name, password, country) values ('$name', '$password', '$country')";
+    $conn->exec($signInUserQuery);
+    echo "Hello $name";
